3112760d85e73f8e9633b86a10ae1b06c5307f2a
Patrick Clay <pclay@google.com>
2015-08-04 14:20:55 -0700

    sad

---
+++ awesome/menu.lua
@@ -0,0 +1,195 @@
+submenu0 =
+{
+  { "Activity Log Manager", "activity-log-manager" },
+  { "Contacts", "gnome-contacts" },
+  { "Files", "nemo" },
+  { "Files", "nautilus --new-window" },
+  { "gedit", "gedit" },
+  { "GNU Emacs 24", "/usr/bin/emacs24" },
+  { "Goobuntu Configuration", "gksudo goobuntu-config-wizard" },
+  { "Goobuntu Indicator", "/usr/share/goobuntu-indicator/goobuntu_indicator.py" },
+  { "GVim", "gvim -f" },
+  { "nitrogen", "nitrogen" },
+  { "Synergy", "synergy" }
+}
+submenu1 =
+{
+  { "gnubbyd", "/opt/google/chrome-beta/google-chrome-beta --user-data-dir=/home/pclay/.config/google-chrome-beta --profile-directory=Default --app-id=beknehfpfkghjoafdifaflglpjkojoco" },
+  { "Google Corporate Extension Reporter", "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=abjoigjokfeibfhiahiijggogladbmfm" },
+  { "Google Keep [Internal Only] CORP", "/opt/google/chrome-beta/google-chrome-beta --user-data-dir=/home/pclay/.config/google-chrome-beta --profile-directory=Default --app-id=clcfogcfhcdeikbgekdjijhkmbinlmol" },
+  { "Google Keep - notes and lists", "/opt/google/chrome-beta/google-chrome-beta --user-data-dir=/home/pclay/.config/google-chrome-beta --profile-directory=Default --app-id=hmjkmjkepdijhoojdojkdfohbdgmmhki" }
+}
+submenu2 =
+{
+  { "Nibbles", "gnome-nibbles" },
+  { "Robots", "gnome-robots" },
+  { "Chess", "gnome-chess" },
+  { "Iagno", "iagno" },
+  { "Mahjongg", "gnome-mahjongg" },
+  { "AisleRiot Solitaire", "/usr/games/sol" },
+  { "Tali", "tali" },
+  { "Quadrapassel", "quadrapassel" }
+}
+submenu3 =
+{
+  { "Five or More", "five-or-more" },
+  { "Four-in-a-Row", "four-in-a-row" },
+  { "Klotski", "gnome-klotski" },
+  { "Lights Off", "lightsoff" },
+  { "Mines", "gnome-mines" },
+  { "Sudoku", "gnome-sudoku" },
+  { "Swell Foop", "swell-foop" },
+  { "Tetravex", "gnome-tetravex" }
+}
+submenu4 =
+{
+  { "GIMP Image Editor", "gimp-2.8" },
+  { "Gpick", "gpick" },
+  { "gv", "gv" },
+  { "ImageMagick (display)", "/usr/bin/display.im6" },
+  { "LibreOffice Draw", "libreoffice --draw" },
+  { "R", "xterm -title \"R\" -e R" },
+  { "Shotwell", "shotwell" },
+  { "Simple Scan", "simple-scan" }
+}
+submenu5 =
+{
+  { "Browser", "webbrowser-app" },
+  { "Empathy", "empathy" },
+  { "Firefox Web Browser", "firefox" },
+  { "Google Chrome", "/usr/bin/google-chrome-stable" },
+  { "Google Chrome (beta)", "/usr/bin/google-chrome-beta" },
+  { "Google Drive", "/opt/google/drive/drive-launcher.sh" },
+  { "Remmina", "remmina" },
+  { "Thunderbird Mail", "thunderbird" },
+  { "Transmission", "transmission-gtk" }
+}
+submenu6 =
+{
+  { "LibreOffice", "libreoffice" },
+  { "LibreOffice Calc", "libreoffice --calc" },
+  { "LibreOffice Draw", "libreoffice --draw" },
+  { "LibreOffice Impress", "libreoffice --impress" },
+  { "LibreOffice Math", "libreoffice --math" },
+  { "LibreOffice Writer", "libreoffice --writer" }
+}
+submenu7 =
+{
+  { "Amazon", "unity-webapps-runner --amazon --app-id=ubuntu-amazon-default" },
+  { "Awesome", "awesome" },
+  { "JProfiler", "/bin/sh \"/home/pclay/src/jprofiler8/bin/jprofiler\"" },
+  { "NX Client for Linux", "/usr/NX/bin/nxclient" },
+  { "NX Connection Wizard", "/usr/NX/bin/nxclient -wizard" },
+  { "NX Help on the Web", "konqueror http://www.nomachine.com/help" },
+  { "NX Session Administrator", "/usr/NX/bin/nxclient -admin" },
+  { "Personal File Sharing", "gnome-file-share-properties" },
+  { "Photos lens for Unity", "" }
+}
+submenu8 =
+{
+  { "Data Display Debugger", "ddd" },
+  { "Eclipse 4.3", "/usr/bin/eclipse43" },
+  { "Eclipse 4.3 (testing)", "/usr/bin/eclipse43_testing" },
+  { "Eclipse 4.4", "/usr/bin/eclipse44" },
+  { "Eclipse 4.4 (testing)", "/usr/bin/eclipse44_testing" },
+  { "GNU Emacs 24", "/usr/bin/emacs24" },
+  { "Kompare", "kompare -caption \"Kompare\" -o" }
+}
+submenu9 =
+{
+  { "LibreOffice Math", "libreoffice --math" }
+}
+submenu10 =
+{
+  { "Brasero", "brasero" },
+  { "Cheese", "cheese" },
+  { "Rhythmbox", "rhythmbox" },
+  { "Sound Recorder", "gnome-sound-recorder" },
+  { "Videos", "totem" }
+}
+submenu11 =
+{
+  { "Configuration Editor", "gconf-editor" },
+  { "dconf Editor", "dconf-editor" },
+  { "Main Menu", "alacarte" },
+  { "Network Connections", "nm-connection-editor" },
+  { "Power Statistics", "gnome-power-statistics" },
+  { "Screen Reader", "orca --replace" }
+}
+submenu12 =
+{
+  { "Background", "gnome-control-center background" },
+  { "Bluetooth", "gnome-control-center bluetooth" },
+  { "Brightness &amp; Lock", "gnome-control-center screen" },
+  { "Color", "gnome-control-center color" },
+  { "Date &amp; Time", "gnome-control-center datetime" },
+  { "Details", "gnome-control-center info" },
+  { "Displays", "gnome-control-center display" },
+  { "Goobuntu Configuration", "gksudo goobuntu-config-wizard" },
+  { "Goobuntu Screensaver Timeout", "gksudo goobuntu-config-wizard goobuntu-gui" },
+  { "Keyboard", "gnome-control-center keyboard" },
+  { "Language Support", "/usr/bin/gnome-language-selector" },
+  { "Mouse &amp; Touchpad", "gnome-control-center mouse" },
+  { "Network", "gnome-control-center network" },
+  { "Online Accounts", "gnome-control-center online-accounts" },
+  { "Power", "gnome-control-center power" },
+  { "Printers", "gnome-control-center printers" },
+  { "Region &amp; Language", "gnome-control-center region" },
+  { "Sound", "gnome-control-center sound" },
+  { "Universal Access", "gnome-control-center universal-access" },
+  { "User Accounts", "gnome-control-center user-accounts" },
+  { "Wacom Tablet", "gnome-control-center wacom" }
+}
+submenu13 =
+{
+  { "NVIDIA X Server Settings", "/usr/bin/nvidia-settings" },
+  { "Software Updater", "/usr/bin/update-manager" },
+  { "Startup Disk Creator", "usb-creator-gtk" },
+  { "Ubuntu Software Center", "/usr/bin/software-center" },
+  { "xdiagnose", "pkexec xdiagnose" }
+}
+submenu14 =
+{
+  { "Additional Drivers", "/usr/bin/software-properties-gtk --open-tab=4" },
+  { "CompizConfig Settings Manager", "ccsm" },
+  { "Disks", "gnome-disks" },
+  { "Input Method", "im-config" },
+  { "Onboard Settings", "onboard-settings" },
+  { "Online Accounts", "credentials-preferences" },
+  { "OpenJDK Java 7 Policy Tool", "/usr/bin/policytool" },
+  { "Passwords and Keys", "/usr/bin/seahorse" },
+  { "Software &amp; Updates", "software-properties-gtk" },
+  { "Startup Applications", "gnome-session-properties" },
+  { "Tweak Tool", "gnome-tweak-tool" },
+  { "Nepomuk Cleaner", "nepomukcleaner" },
+  { "System Settings", "gnome-control-center --overview" },
+  { "System Testing", "checkbox-gui" },
+  { "Terminator", "terminator" },
+  { "UXTerm", "uxterm" },
+  { "XTerm", "xterm" }
+}
+submenu15 =
+{
+  { "Onboard", "onboard" }
+}
+myappmenu =
+{
+  { "Accessories", submenu0 },
+  { "Chrome Apps", submenu1 },
+  { "Games", submenu2 },
+  { "Logic", submenu3 },
+  { "Graphics", submenu4 },
+  { "Internet", submenu5 },
+  { "Office", submenu6 },
+  { "Other", submenu7 },
+  { "Programming", submenu8 },
+  { "Science", submenu9 },
+  { "Sound &amp; Video", submenu10 },
+  { "Sundry", submenu11 },
+  { "System Settings", submenu12 },
+  { "System Tools", submenu13 },
+  { "Administration", submenu14 },
+  { "Preferences", submenu15 },
+  { "Universal Access", submenu16 },
+  { "Utilities", submenu17 }
+}
