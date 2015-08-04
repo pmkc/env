c487e09b0fc28da7c754e808fd3c204d8d687b20
Patrick Clay <pclay@google.com>
2015-08-04 14:01:45 -0700

    dsa

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Shells
+ln -ns ~/env/common/profile ~/.profile
+ln -ns ~/env/bash/rc ~/.bashrc
+ln -ns ~/env/zsh/env ~/.zshenv
+
+# Vim
+ln -ns ~/env/vim/rc ~/.vimrc
+ln -ns ~/env/vim ~/.vim
+
+# Tmux
+ln -ns ~/env/tmux/conf ~/.tmux.conf
+
+# Awesome WM
+ln -ns ~/env/awesome ~/.config/awesome
+
+# misc
+ln -nst ~/.config ~/env/misc/config/*
