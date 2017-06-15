6fff657ebe597e2d59c373d436c3b924154f5e25
Patrick Clay <pclay@google.com>
2017-06-15 13:49:54 -0700

    awesome

--- awesome/gnome/install.sh
+++ awesome/gnome/install.sh
@@ -6,6 +6,6 @@ dpkg -l awesome gnome-session gnome-panel gnome-settings-daemon
 
 DIR=$(dirname $0)
 cp $DIR/awesome.desktop /usr/share/applications
-cp $DIR/gnome-awesome.desktop /usr/share/xsessions
+cp $DIR/awesome-gnome.desktop /usr/share/xsessions
 cp $DIR/awesome.session /usr/share/gnome-session/sessions
-
+find /usr/share/applications/ -exec sed -i 's/OnlyShowIn.*GNOME;/&Awesome GNOME;/' {} \;
