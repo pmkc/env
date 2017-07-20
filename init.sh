0795893e86ea7b95d1038323fb45c91649b2d6c9
Patrick Clay <pclay@google.com>
2017-07-20 13:38:23 -0700

    wip

--- init.sh
+++ init.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Shells
-ln -ns ~/env/common/profile ~/.profile
-ln -ns ~/env/bash/rc ~/.bashrc
-ln -ns ~/env/zsh/env ~/.zshenv
+ln -ns env/common/profile ~/.profile
+ln -ns env/bash/rc ~/.bashrc
+ln -ns env/zsh/env ~/.zshenv
 
 # Vim
-ln -ns ~/env/vim/rc ~/.vimrc
-ln -ns ~/env/vim ~/.vim
+ln -ns env/vim/rc ~/.vimrc
+ln -ns env/vim ~/.vim
+mkdir -p ~/.cache/vim
 
 # Tmux
-ln -ns ~/env/tmux/conf ~/.tmux.conf
+ln -ns env/tmux/conf ~/.tmux.conf
 
 # Awesome WM
-ln -ns ~/env/awesome ~/.config/awesome
+ln -ns env/awesome ~/.config/awesome
 
 # misc
-ln -nst ~/.config ~/env/misc/config/*
+ln -nst .config ~/env/misc/config/*
