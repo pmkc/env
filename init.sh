bb6ecdcbe8c6a718705b202faf2b657bdc46c085
Patrick Clay <pclay@google.com>
2018-02-23 14:02:42 -0800

    wip

--- init.sh
+++ init.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Cleanup
+rm -f ~/.profile ~/.bashrc ~/.zshenv ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.vim ~/.config/awesome ~/.config/user-dirs.dirs
+mkdir -p ~/.cache ~/.config
+
 # Shells
 ln -ns env/common/profile ~/.profile
 ln -ns env/bash/rc ~/.bashrc
 ln -ns env/zsh/env ~/.zshenv
 
 # Vim
-ln -ns env/vim/rc ~/.vimrc
-ln -ns env/vim ~/.vim
 mkdir -p ~/.cache/vim
+ln -ns ~/env/vim/rc ~/.vimrc
+ln -ns ~/env/vim ~/.vim
 
 # Tmux
 ln -ns env/tmux/conf ~/.tmux.conf
