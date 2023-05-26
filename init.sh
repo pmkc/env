245d4596765ebe7f85db2008b686083563458f63
Patrick Clay <pclay@google.com>
2023-05-26 09:04:44 -0700

    wip

--- init.sh
+++ init.sh
@@ -6,8 +6,9 @@ mkdir -p ~/.cache ~/.config ~/bin
 
 # Shells
 ln -nsf env/common/profile ~/.profile
-ln -nsf env/bash/rc ~/.bashrc
 ln -nsf env/zsh/env ~/.zshenv
+touch ~/.bashrc
+ln -nsf env/zsh/rc ~/.zshrc
 
 # Vim
 mkdir -p ~/.cache/vim
@@ -19,6 +20,10 @@ ln -nsf ~/env/tmux/conf ~/.tmux.conf
 
 # Awesome WM
 ln -nsf ~/env/awesome ~/.config/awesome
+
+mkdir -p ~/.local/share/fonts
+cp -r ~/env/fonts/* ~/.local/share/fonts
+fc-cache ~/.local/share/fonts
 
 # misc
 echo ~/env/config/* | xargs -n1 ln -nsft ~/.config
